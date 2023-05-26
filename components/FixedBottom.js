143c7332eeb4e7afaff372b2469a5e97167b4b0d
Idrak Mustahsin <playbox8g@gmail.com>
2023-05-26 18:20:10 +0600

    Fixed deployment error

--- components/FixedBottom.js
+++ components/FixedBottom.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from '../styles/Listings.module.css';
 import Link from 'next/link';
-import { checkout } from '@/pages/payment';
 
 const FixedBottom = ({ data }) => {
   return (
