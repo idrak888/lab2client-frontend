9a8bc9cf25d5f31831a5a9d926c9a8a062ac376d
Idrak Mustahsin <playbox8g@gmail.com>
2023-07-29 14:51:15 +0600

    Revert "slideShow HomePageV2"
    This reverts commit 7221475d174c9a4ecbc712b57ec8a22c1c24f120.

--- components/slideshow2.js
+++ components/slideshow2.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
-const Slideshow2 = ({ interval = 5000 }) => {
+const Slideshow2 = ({ interval = 50000 }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
+  // Define the image URLs here
   const imageUrls = [
     'https://images.pexels.com/photos/3735709/pexels-photo-3735709.jpeg?cs=srgb&dl=pexels-polina-tankilevitch-3735709.jpg&fm=jpg',
     'https://cdn.labmanager.com/assets/articleNo/28148/aImg/50928/june22-bm-optimizing-ehs-1800x900-s.jpg',
@@ -27,50 +28,39 @@ const Slideshow2 = ({ interval = 5000 }) => {
   };
 
   return (
-    <div style={{ position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <button onClick={handleBack} style={arrowButtonStyle}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
-        </svg>
-      </button>
-      <div style={{ width: '70%', height: '70%', position: 'relative' }}>
-        {imageUrls.map((url, index) => (
-          <img
-            key={index}
-            src={url}
-            alt={`Slide ${index + 1}`}
-            style={{
-              display: index === currentSlide ? 'block' : 'none',
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-            }}
-          />
-        ))}
-      </div>
-      <button onClick={handleForward} style={arrowButtonStyle}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M0 0h24v24H0z" fill="none" />
-          <path d="M8.59 16.59L10 18l6-6-6-6-1.41 1.41L13.17 12z" />
-        </svg>
-      </button>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      
+      {imageUrls.map((url, index) => (
+        <img
+          key={index}
+          src={url}
+          alt={`Slide ${index + 1}`}
+          style={{
+            display: index === currentSlide ? 'block' : 'none',
+            width: '70%',
+            height: '70%',
+            
+          }}
+        />
+      ))}
+      
+
     </div>
   );
 };
 
+
+
 const arrowButtonStyle = {
-  backgroundColor: '#0a3394',
+  position: 'absolute',
+  top: '50%',
+  transform: 'translateY(-50%)',
+  backgroundColor: 'transparent',
   border: 'none',
+  fontSize: '24px',
   cursor: 'pointer',
+  color: '#fff',
   zIndex: 1,
-  width: '40px',
-  height: '40px',
-  borderRadius: '50%',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
 };
 
 export default Slideshow2;
