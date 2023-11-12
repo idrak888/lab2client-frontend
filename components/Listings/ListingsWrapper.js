efe88a436b6c4e91de9ee318eb54b3cc602160b3
Idrak Mustahsin <playbox8g@gmail.com>
2023-11-11 19:41:59 -0500

    Fixed css bug, added dynamic equipments (commented out for now)

--- components/Listings/ListingsWrapper.js
+++ components/Listings/ListingsWrapper.js
@@ -12,79 +12,71 @@ export default function ListingsWrapper({ data, edit }) {
 
 	const DynamicWidthElement = ({ text }) => {
 		const dynamicWidth = {
-		  backgroundColor: '#294B80',
-		  borderRadius: '12px',
-		  justifyContent: 'center',
-		  display: 'flex',
-		  color: 'white',
-		  width: 'auto',
-		  padding: "0.8% 2%",
-		  fontSize: '12px',
-		  alignItems: 'center',
-		  fontFamily: 'Segoe UI',
-		  marginBottom: '8px', 
-		  marginRight: "8px"
+			backgroundColor: '#294B80',
+			borderRadius: '12px',
+			justifyContent: 'center',
+			display: 'flex',
+			color: 'white',
+			width: 'auto',
+			padding: "0.8% 2%",
+			fontSize: '12px',
+			alignItems: 'center',
+			marginBottom: '8px',
+			marginRight: "8px"
 		};
-	  
+
 		return <div style={dynamicWidth}>{text}</div>;
-	  };
+	};
 
 	return (
-	  <div className={styles.listings}>
-		{/* Check if there is data to display */}
-		{data.length > 0 ? data.map(listing => {
-		  return (
-			// Link to view or edit page with listing details
-			<Link
-			  href={{
-				pathname: edit ? '/edit' : '/view',
-				query: { id: listing.id },
-			  }}
-			  className={styles.card}
-			  key={listing.id}  // Add a key for each mapped element
-			>
-			  <div className='row' style={{ display: "flex", alignItems: "center" }}>
-				<div className='col-sm-4'>
-				  <img style={{ width: "100%", maxWidth: 250, padding: 0, borderRadius: 25 }} src={listing.research["LOGOS"]} />
-				</div>
-				<div className='col-sm-8'>
-				  <h3 className={styles.title}>{listing.identification.research_facillity}</h3>
-				  <div style={{ marginBottom: "1%" }}>
-					<i style={{ fontSize: "75%", marginRight: "1%" }} className='bi bi-geo-alt'></i>
-					<span className={styles.subtitle}>{listing.identification.institution_name}</span>
-					<i className="bi bi-dot"></i>
-					<span className={styles.subtitle}>{listing.identification.city}, {listing.identification.province}</span>
-				  </div>
-				  <p style={{ textDecoration: "none", fontWeight: "normal" }}>
-					Fields of Research: {listing.Fields_of_research.fields.join(", ")}.
-				  </p>
-				  <h6 className={styles.equipmentsTitle}>Equipment Offered:</h6>
-				  <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-						{items.map((item, index) => (
-							<DynamicWidthElement key={index} text={item} />
-						))}
-						{
-							items.length > 5 ? <DynamicWidthElement key={-1} text={"+9 more"}/> : null
-						}
-				  </div>
-				</div>
-			  </div>
-			</Link>
-		  )
-		}) : null}
-	  </div>
-	)
-  }
-
-  /*
-  <div className={styles.fields}>
-					{listing.Fields_of_research.fields.map(field => {
-					  return (
-						<div className={styles.pill} key={field}>
-						  {field}
+		<div className={styles.listings}>
+			{/* Check if there is data to display */}
+			{data.length > 0 ? data.map(listing => {
+				return (
+					// Link to view or edit page with listing details
+					<Link
+						href={{
+							pathname: edit ? '/edit' : '/view',
+							query: { id: listing.id },
+						}}
+						className={styles.card}
+						key={listing.id}  // Add a key for each mapped element
+					>
+						<div className='row' style={{ display: "flex", alignItems: "center" }}>
+							<div className='col-sm-4'>
+								<img style={{ width: "100%", maxWidth: 250, padding: 0, borderRadius: 25 }} src={listing.research["LOGOS"]} />
+							</div>
+							<div className='col-sm-8'>
+								<h3 className={styles.title}>{listing.identification.research_facillity}</h3>
+								<div style={{ marginBottom: "1%" }}>
+									<i style={{ fontSize: "75%", marginRight: "1%" }} className='bi bi-geo-alt'></i>
+									<span className={styles.subtitle}>{listing.identification.institution_name}</span>
+									<i className="bi bi-dot"></i>
+									<span className={styles.subtitle}>{listing.identification.city}, {listing.identification.province}</span>
+								</div>
+								<p style={{ textDecoration: "none", fontWeight: "normal" }}>
+									Fields of Research: {listing.Fields_of_research.fields.join(", ")}.
+								</p>
+								<h6 className={styles.equipmentsTitle}>Equipment Offered:</h6>
+								<div style={{ display: 'flex', flexWrap: 'wrap' }}>
+									{/* {listing.lab_equipment.map((item, index) => (
+										<DynamicWidthElement key={index} text={item.name} />
+									))}
+									{
+										listing.lab_equipment.length > 5 ? <DynamicWidthElement key={-1} text={"+9 more"} /> : null
+									} */}
+									{items.map((item, index) => (
+										<DynamicWidthElement key={index} text={item} />
+									))}
+									{
+										items.length > 5 ? <DynamicWidthElement key={-1} text={"+9 more"} /> : null
+									}
+								</div>
+							</div>
 						</div>
-					  )
-					})}
-				  </div>
-  */
-  
+					</Link>
+				)
+			}) : null}
+		</div>
+	)
+}
