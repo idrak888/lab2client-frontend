d14b605befb1e22fa0c3b238ffac023b0db1e8d8
4bdulWadood <57127141+4bdulWadood@users.noreply.github.com>
2023-11-21 18:08:45 -0500

    Browse Page UI Update

--- components/Listings/ListingsWrapper.js
+++ components/Listings/ListingsWrapper.js
@@ -1,3 +1,5 @@
+// ListingsWrapper.js
+
 import React from 'react';
 import styles from '/styles/Listings.module.css';
 import Link from 'next/link';
@@ -8,30 +10,43 @@ export default function ListingsWrapper({ data, edit }) {
   const items = ["Autocollimators", "Scanning Electron Microscope (SEM)", "Microscope", "Another Component", "Yet Another Component", "More Components"];
 
   return (
-    <div className={styles.listings}>
+    <div className={`${styles.listings} ${styles.highlighted}`}>
       {/* Check if there is data to display */}
       {data.length > 0 ? data.map(listing => {
         return (
-          // Link to view or edit page with listing details
           <Link
             href={{
               pathname: edit ? '/edit' : '/view',
               query: { id: listing.id },
             }}
             className={styles.card}
-            key={listing.id}  // Add a key for each mapped element
+            key={listing.id}
           >
             <div className={`row ${styles.responsiveCard}`} style={{ display: "flex", alignItems: "center" }}>
               <div className='col-sm-4'>
-                <img style={{ width: "100%", maxWidth: 250, padding: 0, borderRadius: 25 }} src={listing.research["LOGOS"]} />
+                <img
+                  style={{
+                    width: "100%",
+                    height: "200px", // Set your desired fixed height here
+                    objectFit: "cover", // Crop the image to fit the specified dimensions
+                    borderRadius: "25px",
+                  }}
+                  src={listing.research["LOGOS"]}
+                  alt="Research Logo"
+                />
               </div>
-              <div className='col-sm-8'>
+              <div className='col-sm-8 d-flex flex-column'>
                 <h3 className={styles.title}>{listing.identification.research_facillity}</h3>
-                <div style={{ marginBottom: "1%" }}>
-                  <i style={{ fontSize: "75%", marginRight: "1%" }} className='bi bi-geo-alt'></i>
-                  <span className={styles.subtitle}>{listing.identification.institution_name}<i className="bi bi-dot"></i></span>
-                  <span className={styles.subtitle}>{listing.identification.city}, {listing.identification.province}</span>
-                </div>
+                <div className={`${styles.greyComponent} ${styles.componentWrapper}`} style={{ marginBottom: "1%" }}>
+                <i className={`bi bi-geo-alt me-1 fs-6`}></i>
+                <span style={{fontSize: "12px"}}>
+                  {listing.identification.institution_name}
+                </span>
+                <i className={`bi bi-dot fs-6`} style={{ margin: "0 2px" }}></i>
+                <span style={{fontSize: "12px"}} className={`font-size-6 font-weight-light`}>
+                  {listing.identification.city}, {listing.identification.province}
+                </span>
+              </div>
                 <p style={{ textDecoration: "none", fontWeight: "normal" }}>
                   Fields of Research: {listing.Fields_of_research.fields.join(", ")}.
                 </p>
