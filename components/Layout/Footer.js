0abb9eb415425eb7f6b3cf7fc00dd4645209dbdf
4bdulWadood <a1syed@torontomu.ca>
2023-12-20 19:44:56 -0500

    Added Footer

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,41 +1,26 @@
-import React from 'react'
-
-// this is main the footer on the bottom of the page
+import React from 'react';
 
 export default function Footer() {
     return (
         <div className='FooterWrapper'>
-            <footer className="footer text-light py-5">
+            <footer className="footer">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-6 footer-content">
-                            <h5>About Lab2Client</h5>
-                            <p>Lab2Client is an innovative web platform that connects the broader research and innovation
-                                community with under-utilized experimental research facilities and expertise.</p>
+                        <div className="col-lg-4 footer-content">
+                            <h1>Lab2Client</h1>
+                            <p>&copy; 2023 Lab2Client. All rights reserved.</p>
                         </div>
-                        <div className="col-lg-3 footer-links">
-                            <h5>Quick Links</h5>
-                            <ul className="list-unstyled">
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/about">About</a></li>
-                                <li><a href="/">Services</a></li>
-                                <li><a href="/#contact">Contact</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-lg-3 footer-social">
-                            <h5>Follow Us</h5>
-                            <a href="/" target="_blank"><i className="bi bi-facebook"></i></a>
-                            <a href="/" target="_blank"><i className="bi bi-twitter"></i></a>
-                            <a href="/" target="_blank"><i className="bi bi-instagram"></i></a>
+                        <div className="col-lg-6 footer-links">
+                            <div className="d-flex justify-content-end">
+                                <a href="/">Find Labs</a>
+                                <a href="/">About</a>
+                                <a href="/">Contact</a>
+                                <a href="/">Offer your Lab</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </footer>
-            <div style={{width: "100%", backgroundColor: "#0B131C", padding: 20, color: "rgba(255, 255, 255, 0.7)"}}>
-                <div className="col-lg-12 text-center mt-4">
-                    <p>&copy; 2023 Lab2Client. All rights reserved.</p>
-                </div>
-            </div>
         </div>
-    )
+    );
 }
