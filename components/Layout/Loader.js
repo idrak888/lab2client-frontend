b77ce712274651a875f073cabce5ee9ea970dfe5
Idrak Mustahsin <playbox8g@gmail.com>
2023-12-25 13:56:29 -0500

    Fixed loader

--- components/Layout/Loader.js
+++ components/Layout/Loader.js
@@ -4,8 +4,21 @@ import React from 'react'
 
 export default function Loader() {
   return (
-    <svg className='loader' viewBox="25 25 50 50">
-        <circle r="20" cy="50" cx="50"></circle>
+    <svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#102D5D">
+      <g fill="none" fill-rule="evenodd">
+        <g transform="translate(1 1)" stroke-width="2">
+          <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="1s"
+              repeatCount="indefinite" />
+          </path>
+        </g>
+      </g>
     </svg>
   )
 }
